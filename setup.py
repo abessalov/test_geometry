ce7e74c77d51b8a2965565f645f4e64481bef492
Andrey <a.bessalov@gmail.com>
2023-09-25 16:45:26 +0300

    updates

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'test_geometry',
+    version = '0.0.1',
+    author = 'Andrey Bessalov',
+    author_email = 'a.bessalov@gmail.com',
+    description = 'Package for testing pip uploading',
+    packages = ['test_geometry'],
+    zip_safe = False
+)
